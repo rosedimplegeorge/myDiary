27ebff8450f24f4e05f51d530c8119e09b511ee2
rosedimple <rosedimplegeorge@gmail.com>
2018-05-29 19:04:04 -0400

    Index Route created successfully for the second model and tested on POSTMAN

--- app/controllers/api/procedures_controller.rb
+++ app/controllers/api/procedures_controller.rb
@@ -1,2 +1,9 @@
 class Api::ProceduresController < ApplicationController
+
+    def index
+        @procedures = Recipe.find(params[:recipe_id]).procedures
+        render json: @procedures
+    end
+
+
 end
