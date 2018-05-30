748eb2a1b093cb8d0d808de001f94eecfde8b1b4
rosedimple <rosedimplegeorge@gmail.com>
2018-05-30 12:17:54 -0400

    Landing page is created and is responsive

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
+import { Jumbotron } from 'react-bootstrap';
+import { Image } from 'react-bootstrap';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Jumbotron>
+            <h1>My Diary</h1>
+            <p>
+              <Button bsStyle="primary">Recipes</Button>
+            </p>
+        </Jumbotron>
+        <Image src="https://i.imgur.com/xo1P2k0.jpg" responsive />
       </div>
     );
   }
