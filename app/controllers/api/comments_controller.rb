26d8083ebea715c0faf4c8c52b6c79312b1a04b4
rosedimple <rosedimplegeorge@gmail.com>
2018-05-30 09:25:39 -0400

    Comments Controller created and Index Route is created and configured successfully and tested on Postman

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,6 +1,9 @@
 class Api::CommentsController < ApplicationController
 
-  
+    def index
+        @comments = Recipe.find(params[:recipe_id]).comments
+        render json: @comments
+    end
 
 
 end
