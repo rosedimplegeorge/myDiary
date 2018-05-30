c270b07405c4d9f75fac0abbbccf72b2e796b605
rosedimple <rosedimplegeorge@gmail.com>
2018-05-30 09:24:20 -0400

    CREATED AND EDIT Routes are created

--- app/controllers/api/procedures_controller.rb
+++ app/controllers/api/procedures_controller.rb
@@ -16,4 +16,23 @@ class Api::ProceduresController < ApplicationController
         render status: 200
     end
 
+    def create
+        @recipe = Recipe.find(params[:recipe_id])
+        @procedure = Procedure.create!(procedure_params)
+        render json: @procedure
+    end
+
+    def update
+        @recipe = Recipe.find(params[:recipe_id])
+        @procedure = Procedure.find(params[:id])
+        @procedure.update!(procedure_params)
+        render json: @procedure
+    end
+
+    private
+
+    def procedure_params
+        params.require(:procedure).permit(:title, :description, :photo_url, :recipe_id)
+    end
+
 end
